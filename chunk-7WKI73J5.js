9d9370be943a322bfa78a046d739b17c93e4d8c8
wesley-c-f-lourenco <wesleywcfl@gmail.com>
2026-05-16 12:49:20 -0300

    Auto-generated commit

---
+++ chunk-7WKI73J5.js
@@ -0,0 +1,1 @@
+import"./chunk-FDERIQAA.js";var t=[{path:"",loadComponent:()=>import("./chunk-KCRXPHYE.js").then(o=>o.DashboardComponent)}];export{t as DASHBOARD_ROUTES};
--- chunk-YM5AJTZL.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-FDERIQAA.js";var t=[{path:"",loadComponent:()=>import("./chunk-5KV35ZAR.js").then(o=>o.DashboardComponent)}];export{t as DASHBOARD_ROUTES};
